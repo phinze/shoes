fc5675227e5cf79fa566bec8c3f3a2f9369b6a20
Chuck Remes <cremes@mac.com>
2011-09-26 15:03:05 -0500

    debug msg

--- make/mingw/env.rb
+++ make/mingw/env.rb
@@ -48,7 +48,7 @@ LINUX_LDFLAGS = " -DBUILD_DLL -lungif -ljpeg -lglib-2.0 -lgobject-2.0 -lgio-2.0 
 LINUX_LDFLAGS << ' -lshell32 -lkernel32 -luser32 -lgdi32 -lcomdlg32 -lcomctl32 -lole32 -loleaut32 -ladvapi32 -loleacc -lwinhttp'
 
 cp APP['icons']['win32'], "shoes/appwin32.ico"
-echo %{echo '"$PWD"'}
+sh %{echo '"$PWD"'}
   
 LINUX_LIBS = LINUX_LIB_NAMES.map { |x| "-l#{x}" }.join(' ')
 
