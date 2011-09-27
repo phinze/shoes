ac75e862aaaff8e8af25d11abd0e948b200fa6c7
Chuck Remes <cremes@mac.com>
2011-09-27 17:32:02 -0500

    change extconf.rb for binject to find zlib.h in correct place

--- req/binject/ext/binject_c/extconf.rb
+++ req/binject/ext/binject_c/extconf.rb
@@ -2,6 +2,9 @@ require 'mkmf'
 
 $CFLAGS << " -Iincludes "
 $CFLAGS << "-DRUBY_#{RUBY_VERSION[0..2].sub(/\./,'_')} "
-$CFLAGS << "-DSHOES_MINGW32 " if RUBY_PLATFORM =~ /mingw/
+if RUBY_PLATFORM =~ /mingw/
+  $CFLAGS << " -I../../../../../zlib/include "
+  $CFLAGS << "-DSHOES_MINGW32 "
+end
 have_library("z")
 create_makefile("binject")
